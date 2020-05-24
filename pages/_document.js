064592a2e7b9cafb436c16d797bf577d86d2eace
msmichellegar <msmichellegar@gmail.com>
2020-05-24 17:36:45 +0100

    add meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,46 @@
 import Document, { Head, Main, NextScript } from 'next/document';
+import MetaTags from '../components/meta-tags';
 
 const GA_TRACKING_ID = 'UA-64172720-4';
 
 export default class MyDocument extends Document {
   render() {
+    const title = 'Solve a Crossword';
+    const description =
+      'Find answers to crosswords and word puzzles. This crossword solver will suggest words based on letters and length.';
+
+    const url = 'www.solveacrossword.com';
+
+    const image = 'www.solveacrossword.com/static/meta-image.jpg';
+    const imageAlt = 'An open dictionary with answers to crossword puzzles.';
+
+    const twitterHandle = 'msmichellegar';
+
+    const metaTags = (
+      <>
+        <title>{title}</title>
+        <meta property="og:title" content={title} />
+        <meta name="description" content={description} />
+        <meta property="og:site_name" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content={image} />
+        <meta property="og:image:alt" content={imageAlt} />
+        <meta property="og:url" content={url} />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:image:alt" content={imageAlt} />
+        <meta name="twitter:image" content={image} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:creator" content={twitterHandle} />
+        <meta name="twitter:site" content={twitterHandle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </>
+    );
+
     return (
       <html>
         <Head>
+          {metaTags}
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
